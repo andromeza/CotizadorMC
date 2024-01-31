822dcb55887ec14cd0ff6319f75fa9eead05d15b
andromeza <98124924+andromeza@users.noreply.github.com>
2024-01-30 21:27:59 -0600

    feature(HTMLBody)
    Creación de la estructura HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CotizadorMC';
+  title = 'Cotizador';
+  items: any[] = [];
+  prices: any[] = [];
+  discount: any[] = [];
+  priceSelected: any;
+  discountSelected: any;
+  dateE: any;
+  dateS: any;
+
+  constructor(){
+    this.items = [
+      { label: 'Nuevo Precio', icon: 'pi pi-fw pi-plus', command: () => this.handleNuevo() },
+      { label: 'Editar Precio', icon: 'pi pi-fw pi-pencil', command: () => this.handleEditar() },
+      // Puedes agregar más opciones según tus necesidades
+    ];
+
+    this.prices = [
+      { label: 'Raza Pequeña', value: '490'},
+      { label: 'Raza Mediana y Grande', value: '590'}
+    ];
+
+    this.discount = [
+      { label: '10%', value: '10' },
+      { label: '20%', value: '20' },
+      { label: '30%', value: '30' },
+      { label: '40%', value: '40' },
+      { label: '50%', value: '50' }
+    ];
+  }
+
+  handleNuevo() {
+    // Lógica para la opción Nuevo
+    console.log('Nuevo clicked');
+  }
+
+  handleEditar() {
+    // Lógica para la opción Editar
+    console.log('Editar clicked');
+  }
+
+  handleChange(event: any) {
+    console.log('Opción seleccionada:', event.value);
+    // Puedes realizar acciones adicionales aquí si es necesario
+  }
+
+  visible: boolean = false;
+
+    showDialog() {
+        this.visible = true;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MenubarModule } from 'primeng/menubar';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { DialogModule } from 'primeng/dialog';
+
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +16,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MenubarModule,
+    DropdownModule,
+    CalendarModule,
+    DialogModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
